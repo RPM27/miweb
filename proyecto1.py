297ea70df40b823e38de1748839d960dd1e75e2e
Rusbel Palacios <rusbelpalaciosmatos@gmail.com>
2023-01-24 15:06:31 -0500

    Devolviendo el nombre del archivo

---
+++ proyecto1.py
@@ -0,0 +1,10 @@
+print("Buenos dias- tardes noches")
+print("--------------------------")
+print("Ingrese un numero entero: ")
+a=int(input())
+print("Ingrese otro numero entero: ")
+b=int(input())
+c=a*b
+print("Resultado: ",c)
+if c == 3:
+print("")
